db94617e399097244a8f6d1a14befd70365ab70b
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-08-26 00:17:19 +0530

    basic search engine with search functionality complete along with unit tests for search engine

--- database/searchEngine.js
+++ database/searchEngine.js
@@ -21,6 +21,8 @@ class SearchEngine {
         if (Array.isArray(data)) {
             data.forEach(item => {
                 let doc = Object.assign({}, item);
+                delete doc['name'];
+                doc.title = item.name;
                 doc.id = ++this.documentId;
                 this.documents.push(doc);
             });
@@ -31,13 +33,108 @@ class SearchEngine {
         this.documents.push(doc);
     }
 
+    /**
+     * Created a timeStamp based sorted document array
+     */
     createDocumentIndexBasedOnTime() {
         this.sortedIndex = this.documents.map(item => {
                 return {id: item.id, timeStamp: new Date(item.dateLastEdited).getTime()};
             });
         // sort the documents based on timestamps.
         this.sortedIndex.sort((a, b) => a.timeStamp - b.timeStamp);
-        console.log(`sorted document index`, this.sortedIndex);
+    }
+
+    /**
+     * Create inverted index for words and phrases to their respective documents.
+     */
+    createInvertedTextIndex() {
+        this.documents.forEach(document => {
+            let titleTokens       = document.title.toLowerCase().replace(/[.:!]/g, '').split(' ');
+            let descriptionTokens = document.description.toLowerCase().replace(/[.:!]/g, '').split(' ');
+            titleTokens.forEach(token => {
+                if (this.invertedIndex.hasOwnProperty(token)) {
+                    this.invertedIndex[token].add(document.id);
+                }
+                else {
+                    this.invertedIndex[token] = new Set([document.id]);
+                }
+            });
+
+            descriptionTokens.forEach(token => {
+                if (this.invertedIndex.hasOwnProperty(token)) {
+                    this.invertedIndex[token].add(document.id);
+                }
+                else {
+                    this.invertedIndex[token] = new Set([document.id]);
+                }
+            });
+            this.addPhrasesToInvertedIndex(titleTokens, descriptionTokens, document.id);
+        });
+    }
+
+    addPhrasesToInvertedIndex(titleTokens, descriptionTokens, documentId) {
+        // let us now add phrases to inverted index
+        let totalTitleTokens        = titleTokens.length;
+        let totalDescriptionTokens  = descriptionTokens.length;
+
+        // min length of a phrase is 2
+        const minPhraseSize = 2;
+        // max supported phrase length is 4
+        const maxPhraseSize = 4;
+
+        let currentPhraseSize = maxPhraseSize;
+        while(currentPhraseSize >= minPhraseSize) {
+            for(let i = 0; i <= (totalTitleTokens - currentPhraseSize); i++) {
+                let phrase = titleTokens.slice(i, i + currentPhraseSize).join(' ');
+                if (this.invertedIndex.hasOwnProperty(phrase)) {
+                    this.invertedIndex[phrase].add(documentId);
+                }
+                else {
+                    this.invertedIndex[phrase] = new Set([documentId]);
+                }
+            }
+            currentPhraseSize--;
+        }
+        currentPhraseSize = maxPhraseSize;
+        while(currentPhraseSize >= minPhraseSize) {
+            for(let i = 0; i <= (totalDescriptionTokens - currentPhraseSize); i++) {
+                let phrase = descriptionTokens.slice(i, i + currentPhraseSize).join(' ');
+                if (this.invertedIndex.hasOwnProperty(phrase)) {
+                    this.invertedIndex[phrase].add(documentId);
+                }
+                else {
+                    this.invertedIndex[phrase] = new Set([documentId]);
+                }
+            }
+            currentPhraseSize--;
+        }
+    }
+
+    /**
+     * Search for given words and return the matching document(s);
+     * @param {array} keyWords 
+     */
+    searchKeywords(keyWords) {
+        if (!Array.isArray(keyWords)) {
+            return Promise.reject(new Error(`Please supply an array as a parameter`));
+        }
+        let resultIds = new Set();
+        let resultSet = {total: 0, documents: []};
+        keyWords.forEach(key => {
+            let searchTerm = key.toLowerCase();
+            if (this.invertedIndex.hasOwnProperty(searchTerm)) {
+                this.invertedIndex[searchTerm].forEach(item => {
+                    resultIds.add(item);
+                });
+            }
+        });
+
+        resultIds.forEach(id => {
+            resultSet.documents.push(this.documents[id - 1]);
+        });
+        resultSet.total = resultIds.size;
+
+        return resultSet;
     }
 }
 
--- database/tests/index.js
+++ database/tests/index.js
@@ -9,28 +9,61 @@ const mockData = require('../../data/mock_data.json');
  */
 
 describe('Search Engine basic functionality tests', () => {
-
-    describe('Loading mock data inside engine', () => {
-      it('Should load all 100 items present in mock data', () => {
-        const engine = new SearchEngine();
+  const engine = new SearchEngine();
+  
+  describe('Loading mock data inside engine', () => {
+    it('Should load all 100 items present in mock data', () => {
         engine.loadDataIntoDb(mockData);
         assert.equal(engine.documents.length, mockData.length);
       });
 
       it('Documents should have auto incrementing id starting with 1 till total documents length', () => {
-        const engine = new SearchEngine();
-        engine.loadDataIntoDb(mockData);
-        let docId = 1; // initial doc id
+        let documentId = 1; // initial doc id
         engine.documents.forEach(doc => {
-          assert.equal(doc.id, docId);
-          docId++; // increment doc id
+          assert.equal(doc.id, documentId);
+          documentId++; // increment doc id
+        });
+      });
+
+      it('Every document must have id, title, image, description and dateLastEdited', () => {
+        let documentId = 1; // initial doc id
+        engine.documents.forEach(doc => {
+          assert.equal(doc.hasOwnProperty('id'), true);
+          assert.equal(doc.hasOwnProperty('title'), true);
+          assert.equal(doc.hasOwnProperty('image'), true);
+          assert.equal(doc.hasOwnProperty('description'), true);
+          assert.equal(doc.hasOwnProperty('dateLastEdited'), true);
+          documentId++; // increment doc id
         });
       });
 
       it('Auto increment document id inside engine must equal to total document length', () => {
-        const engine = new SearchEngine();
-        engine.loadDataIntoDb(mockData);
         assert.equal(engine.documents.length, engine.documentId);
+      });
+
+      it('SortedIndex must be sorted in ascending order', () => {
+        engine.createDocumentIndexBasedOnTime();
+        const isSorted = true;
+        const indexLength = engine.sortedIndex.length;
+        for (let i = indexLength - 1; i >= 1; i--) {
+          if (engine.sortedIndex[i] < engine.sortedIndex[i - 1]) {
+            isSorted = false;
+            break;
+          }
+          //console.log(`index[${i}] > index[${i-1}] = ${engine.sortedIndex[i].timeStamp} > ${engine.sortedIndex[i-1].timeStamp}`);
+        }
+        assert.equal(isSorted, true);
+      });
+
+      it('Search should return 2 documents for search term `the lion king` ', () => {
+        engine.createInvertedTextIndex();
+        let result = engine.searchKeywords(['the lion king']);
+        assert.equal(result.hasOwnProperty('total'), true);
+        assert.equal(result.hasOwnProperty('documents'), true);
+        assert.equal(Array.isArray(result.documents), true);
+        assert.equal(result.total, 2);
+        //console.log(engine.invertedIndex);
+        //console.log({result: JSON.stringify(result)});
       });
 
     });
