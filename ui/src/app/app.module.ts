5372e4696b718d7ece72aa693c00765db33a26b4
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-09-06 09:52:36 +0530

    UI components: search, sort, card and table components

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FeedCardComponent } from './feed-card/feed-card.component';
+import { SearchComponent } from './search/search.component';
+import { SortComponent } from './sort/sort.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedCardComponent,
+    SearchComponent,
+    SortComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
