8004544a91e6476d90892058362ed04e70b3faab
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-09-08 03:53:50 +0530

    basic e2e tests added

--- ui/e2e/src/app.e2e-spec.ts
+++ ui/e2e/src/app.e2e-spec.ts
@@ -1,16 +1,94 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, Key, protractor } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Feed App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('ui app is running!');
+  describe('UI elements', () => {
+    it('should display Feed as app title', () => {
+      page.navigateTo();
+      expect(page.getTitleText()).toEqual('Feed');
+    });
+  
+    it('should display Search input with empty initial value', () => {
+      //page.navigateTo();
+      expect(page.getSearchInput()).toEqual('');
+    });
+  
+    it('should display Sort select input', () => {
+      expect(page.getSortInput()).toEqual('select');
+    });
+  
+    it('should display 6 feed cards on page load', async () => {
+      const feedCards = await page.getFeedCards();
+      expect(feedCards.length).toEqual(6);
+    });
+  });
+
+  describe('feed cards', () => {
+    it('first feed card must have title `Chief Brand Orchestrator`', async () => {
+      const feedCardTitles = await page.getFeedCards();
+      expect(feedCardTitles[0].getText()).toEqual(`Chief Brand Orchestrator`);
+    });
+  
+    it('last feed card must have title `Investor Quality Executive`', async () => {
+      const feedCardTitles = await page.getFeedCards();
+      expect(feedCardTitles[feedCardTitles.length - 1].getText()).toEqual(`Investor Quality Executive`);
+    });
+  });
+
+  describe('search titles', () => {
+    it('search term `king` should return 4 cards', async () => {
+      const searchInput = page.getSearchWebElement();
+      searchInput.sendKeys('king');
+      searchInput.sendKeys(Key.ENTER);
+      await protractor.promise.delayed(1000);
+      const feedCardTitles = await page.getFeedCards();
+      expect(feedCardTitles.length).toEqual(4);
+    });
+
+    it('search term `king` should match the given titles (default filters)', async () => {
+      page.navigateTo();
+      const titlesToMatch = [
+        'The Lion King',
+        'Human Web Agent',
+        'The Lord of the Rings: The Return of the King',
+        'District Solutions Orchestrator'
+      ];
+      const searchInput = page.getSearchWebElement();
+      await searchInput.sendKeys('king');
+      await searchInput.sendKeys(Key.ENTER);
+      await protractor.promise.delayed(1000);
+      const feedCardTitles = await page.getFeedCards();
+      expect(feedCardTitles.length).toEqual(4);
+      feedCardTitles.forEach((cardTitle, index) => {
+        const titleToMatch = titlesToMatch[index].length > 32 ? titlesToMatch[index].substr(0, 32) + '..' : titlesToMatch[index];
+        expect(cardTitle.getText()).toEqual(titleToMatch);
+      });
+    });
+  });
+
+  describe('Pagination tests', () => {
+    xit('should not navigate to 2nd page with search term `king`', async () => {
+      page.navigateTo();
+      const searchInput  = page.getSearchWebElement();
+      const pageInput    = page.getPageInputWebElement();
+      const jumpToButton = page.getJumpToPageButton();
+
+      searchInput.sendKeys('king');
+      searchInput.sendKeys(Key.ENTER);
+      await protractor.promise.delayed(1000);
+      pageInput.sendKeys(2);
+      pageInput.sendKeys(Key.ENTER);
+      await jumpToButton.click();
+      await protractor.promise.delayed(1000);
+      const feedCardTitles = await page.getFeedCards();
+      expect(feedCardTitles.length).toEqual(0);
+    });
   });
 
   afterEach(async () => {
