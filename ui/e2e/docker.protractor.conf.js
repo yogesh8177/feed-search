3bad1090d3231216412e744a8fe374815e26429b
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-09-09 17:21:13 +0530

    browser option added

--- ui/e2e/docker.protractor.conf.js
+++ ui/e2e/docker.protractor.conf.js
@@ -21,7 +21,7 @@ exports.config = {
         "--disable-gpu", 
         //"--remote-debugging-port=9222",
         "--no-sandbox",
-        //"--disable-dev-shm-usage"
+        "--disable-dev-shm-usage"
       ]
     }
   },
