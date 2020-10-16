03ee26d9489b9e1f8651419c07739069e7912592
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-10-16 16:54:33 +0530

    feat: pass data to sort component dynamically

--- ui/src/app/sort/sort.component.ts
+++ ui/src/app/sort/sort.component.ts
@@ -12,6 +12,39 @@ export class SortComponent implements OnInit {
 
   constructor() { }
 
+  optionGroups: any[] = [
+    {
+      label: 'Last Edited',
+      values: [
+        {
+          id: 1,
+          value: 'dateLastEdited:Date:asc',
+          title: 'Edited (asc)'
+        },
+        {
+          id: 2,
+          value: 'dateLastEdited:Date:desc',
+          title: 'Edited (desc)'
+        }
+      ]
+    },
+    {
+      label: 'Title',
+      values: [
+        {
+          id: 3,
+          value: 'title:string:asc',
+          title: 'Title (asc)'
+        },
+        {
+          id: 4,
+          value: 'title:string:desc',
+          title: 'Title (desc)'
+        }
+      ]
+    }
+  ];
+
   ngOnInit(): void {
   }
 
