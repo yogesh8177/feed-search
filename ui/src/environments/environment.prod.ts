977d297179ee48cf96e52050654ccbaeecd86566
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-10-20 23:46:44 +0530

    api urls updated for staging env

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  feedApiUrl: 'https://api.thesuperheroes.in/feed',
-  feedConfigApiUrl: 'https://api.thesuperheroes.in/config',
+  feedApiUrl: 'https://heroes-search.herokuapp.com/feed',
+  feedConfigApiUrl: 'https://heroes-search.herokuapp.com/config',
   production: true
 };
