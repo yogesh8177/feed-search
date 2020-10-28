457d9ef18a4e1508f7f4821b2d4193d9deb06468
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-10-28 12:22:01 +0530

    feat: voting-service created v1

---
+++ voting-service/index.js
@@ -0,0 +1,121 @@
+const http          = require("http");
+const querystring   = require('querystring');
+const redis         = require('redis');
+const util          = require('./util');
+const { promisify } = require("util");
+const fs            = require('fs');
+
+const REDIS_HOST  = util.fetchEnvVariable('REDIS_HOST');
+const REDIS_PORT  = util.fetchEnvVariable('REDIS_PORT');
+const HOST        = util.fetchEnvVariable('HOST');
+const PORT        = util.fetchEnvVariable('PORT');
+const redisPrefix = util.NODE_ENV;
+
+const buildVersion = fs.readFileSync('./build.txt').toString('utf-8');
+
+const client = redis.createClient({
+    host: REDIS_HOST,
+    port: REDIS_PORT
+});
+
+const incrByAsync = promisify(client.incrby).bind(client);
+
+const fetchVotesController = async (req, res) => {
+    try {
+        await util.validateRequestMethod(req, 'GET');
+
+        const queryParams = querystring.parse(req.url.split('?')[1]);
+        const { voteeIds = '' } = queryParams;
+        let playerIds = voteeIds.split(',');
+
+        const multi = client.multi();
+
+        playerIds.forEach(id => {
+            multi.get(`${redisPrefix}:vote:${id}`);
+        })
+
+        let redisResponse = await new Promise((res, rej) => {
+            multi.exec((error, data) => {
+                if (error) return rej(error);
+                return res(data);
+            });
+        });
+
+        let votesResponse = playerIds.map((id, index) => {
+            return {
+                voteeId: id,
+                count: redisResponse[index]
+            };
+        });
+        
+        util.successResponse(res, {response: votesResponse, buildVersion});
+    }
+    catch(error) {
+        util.logError('Error while fetching votes', { error, buildVersion });
+        util.errorResponse(res, {error, buildVersion});
+    }
+};
+
+const castVoteController = async (req, res) => {
+    try {
+        let response = {};
+        await util.validateRequestMethod(req, 'POST');
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString(); // convert Buffer to string
+        });
+        req.on('end', async () => {
+            console.log(body);
+            let jsonBody = JSON.parse(body);
+            await incrByAsync(`${redisPrefix}:vote:${jsonBody.voteeId}`, 1);
+            response = jsonBody;
+            response.message = 'vote casted';
+            util.successResponse(res, {response, buildVersion});
+        });
+    }
+    catch(error) {
+        util.logError('Error while casting vote', { error, buildVersion });
+        util.errorResponse(res, {error, buildVersion});
+    }
+};
+
+const requestListener = async (req, res) => {
+    const url = req.url.split('?');
+    
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
+
+    switch(url[0]) {
+
+        case '/':
+            res.writeHead(200);
+            res.end(JSON.stringify({service: 'voting-service', status: 'live', buildVersion}));
+        break;
+
+        case '/fetch-votes':
+            return fetchVotesController(req, res);
+        break;
+
+        case '/cast-vote':
+            return castVoteController(req, res);
+        break;
+
+        case '/test':
+            res.writeHead(200);
+            res.end(JSON.stringify({message: 'healthy', buildVersion})); 
+        break;
+
+        default:
+            res.writeHead(404);
+            res.end(`{"message": "Requested resource not found", buildVersion: "${buildVersion}}"`);
+        break;
+    }
+};
+
+const server = http.createServer(requestListener);
+server.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
+});
+
+module.exports = server;
---
+++ voting-service/util.js
@@ -0,0 +1,48 @@
+const dotenv = require('dotenv').config();
+
+const NODE_ENV = process.env.NODE_ENV;
+
+function fetchEnvVariable(variableName) {
+    return (NODE_ENV === 'test') ? dotenv.parsed[variableName] : process.env[variableName];
+}
+
+const validateRequestMethod = (req, method) => {
+    if (req.method !== method) return Promise.reject(new Error(`method: ${req.method} not supported`));
+    return Promise.resolve();
+};
+
+const successResponse = (res, payload) => {
+    const { response, buildVersion } = payload;
+    res.writeHead(200);
+    res.end(JSON.stringify({data: response, buildVersion}));
+}
+
+const errorResponse = (res, errorObject) => {
+    const {error, buildVersion} = errorObject;
+    res.writeHead(500);
+    res.end(JSON.stringify({error: error.message, buildVersion}));
+}
+
+const logError = (message, error) => {
+    console.error({
+        message,
+        error
+    });
+};
+
+const logSuccess = (message, payload) => {
+    console.log({
+        message,
+        payload
+    });
+}
+
+module.exports = {
+    fetchEnvVariable,
+    logError,
+    logSuccess,
+    validateRequestMethod,
+    successResponse,
+    errorResponse,
+    NODE_ENV
+};
