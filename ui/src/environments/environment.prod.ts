04fd0bea4ab45a3c38f6a351b0ad541e6d59af93
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-10-28 16:50:54 +0530

    prod urls updated

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  feedApiUrl: 'https://heroes-search.herokuapp.com/feed',
-  feedConfigApiUrl: 'https://heroes-search.herokuapp.com/config',
+  feedApiUrl: 'https://api.thesuperheroes.in/feed',
+  feedConfigApiUrl: 'https://api.thesuperheroes.in/config',
   liveMatchUrl: 'https://heroes-search.herokuapp.com/live-match',
   fetchVotesUrl: 'https://heroes-voting-service.herokuapp.com/fetch-votes',
   castVoteUrl: 'https://heroes-voting-service.herokuapp.com/cast-vote',
