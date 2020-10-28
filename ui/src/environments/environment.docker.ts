ae6bd57a324ea2aa8f48c78d0c01c851f5e56818
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-10-28 12:25:07 +0530

    environments updated for voting service

--- ui/src/environments/environment.docker.ts
+++ ui/src/environments/environment.docker.ts
@@ -2,6 +2,8 @@ export const environment = {
     feedApiUrl: 'http://web:8000/feed',
     feedConfigApiUrl: 'http://web:8000/config',
     liveMatchUrl: 'http://web:8000/live-match',
+    fetchVotesUrl: 'http://voting-service:3000/fetch-votes',
+    castVoteUrl: 'http://voting-service:3000/cast-vote',
     production: false
 };
   
--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   feedApiUrl: 'https://heroes-search.herokuapp.com/feed',
   feedConfigApiUrl: 'https://heroes-search.herokuapp.com/config',
   liveMatchUrl: 'https://heroes-search.herokuapp.com/live-match',
+  fetchVotesUrl: 'https://heroes-search.herokuapp.com/fetch-votes',
+  castVoteUrl: 'https://heroes-search.herokuapp.com/cast-vote',
   production: true
 };
