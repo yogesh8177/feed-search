f3a68f05d8e865da98fc624e4ebb67fab3d4e52f
Yogesh Jadhav <yogesh.jadhav8177@gmail.com>
2020-10-30 11:01:21 +0530

    Feature/ui superheroes/live match winner display (#113)
    * feat: display match winner based on live-match endpoint response
    
    * close live match window if its unavailable

--- ui/src/app/models/LiveMatch.ts
+++ ui/src/app/models/LiveMatch.ts
@@ -2,7 +2,9 @@ import { Feed } from './Feed';
 
 export class LiveMatch {
     title: string;
+    status: string;
     players: Feed[];
     expires: string;
     startedOn: string;
+    winnerId: string;
 }
