a5348dd75c252848878a29cfd509417378bf4dd5
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-11-01 05:56:00 +0530

    auto-complete wip

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   feedApiUrl: 'https://heroes-search.herokuapp.com/feed',
+  autoCompleteUrl: 'https://heroes-search.herokuapp.com/auto-complete',
   feedConfigApiUrl: 'https://heroes-search.herokuapp.com/config',
   liveMatchUrl: 'https://heroes-search.herokuapp.com/live-match',
   fetchVotesUrl: 'https://heroes-voting-service.herokuapp.com/fetch-votes',
