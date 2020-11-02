299610379b1fb94cb801fd4396359ef5a72593ca
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-11-02 18:28:49 +0530

    auto-complete debounce added along with switchMap to honour latest api call

--- ui/src/app/search/search.component.ts
+++ ui/src/app/search/search.component.ts
@@ -1,45 +1,52 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, EventEmitter, Output, Input } from '@angular/core';
 import { SearchService } from '../Services/search/search.service';
 import { AutoCompleteResults } from '../models/Search';
+import { BehaviorSubject } from 'rxjs';
+import { switchMap, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
   
   @Input() searchText: string = '';
   @Output() searchTerm = new EventEmitter<string>();
   autoCompleteResults: AutoCompleteResults;
+  searchSubject = new BehaviorSubject<string>('');
 
   constructor(private searchService: SearchService) { }
 
   ngOnInit(): void {
     this.autoCompleteResults = new AutoCompleteResults();
     this.autoCompleteResults.data = [];
+    this.subscribeToSearchSubject();
+  }
+
+  ngOnDestroy(): void {
+    this.searchSubject.unsubscribe();
+  }
+
+  subscribeToSearchSubject() {
+    this.searchSubject.pipe(
+      debounceTime(500),
+      switchMap(value => this.searchService.autoComplete(value))
+    )
+    .subscribe(
+      results => {
+        this.autoCompleteResults = results;
+        console.log(`loaded from switchmap`)
+      },
+      error => console.error(error)
+    );
   }
 
   setSearchTerm(event) {
-    console.log('search term: ', event);
+    //console.log('search term: ', event);
+    this.autoCompleteResults.data.length = 0;
     this.searchText = event;
     this.searchTerm.emit(event);
-  }
-
-  triggerAutoComplete(prefix: string) {
-    if (prefix.length === 0) {
-      this.autoCompleteResults.data.length = 0;
-      return;
-    }
-    if (prefix.length > 1) {
-      this.searchService.autoComplete(prefix).subscribe(
-        results => {
-          this.autoCompleteResults = results;
-          //console.log(`autocomplete result`, this.autoCompleteResults);
-        },
-        error => console.error(error)
-      );
-    }
   }
 
   onSuggestionSelect(suggestion: string) {
