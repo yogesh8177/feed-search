e412075ac58536f4c4834e62c0f7a3911c3d5bff
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-11-05 13:44:19 +0530

    require json files

--- api-service/app.ts
+++ api-service/app.ts
@@ -4,7 +4,7 @@ const dotenv = dotEnv.config();
 import GlobalOptions from './models/GlobalOptions';
 
 import { SearchEngine, EngineOptions } from './database/searchEngine';
-import * as mockData from './data/mock_data.json';
+const mockData = require('./data/mock_data.json');
 import * as AWS  from 'aws-sdk';
 import * as fs from 'fs';
 import * as Controllers from './controllers/controllers';
