ebe03689d44d5595e77d88565869bd96b8cf10fa
yogesh8177 <yogesh.jadhav8177@gmail.com>
2021-07-17 23:17:51 +0530

    fix blog page decorator

--- ui/src/app/blog-page/blog-page.component.ts
+++ ui/src/app/blog-page/blog-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
+@Component({
+    selector: 'blog-page',
+    templateUrl: './blog-page.component.html',
+    styleUrls: ['./blog-page.component.scss']
+  })
 export class BlogPageComponent implements OnInit {
     ngOnInit(): void {
     }
