1169f028071c22e6e54fc8eabf9179589a022449
yogesh8177 <yogesh.jadhav8177@gmail.com>
2021-07-18 17:14:51 +0530

    add blog component, routing module

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MyHttpInterceptor } from './Interceptor/httpInterceptor';
 
@@ -14,6 +13,8 @@ import { LoaderComponent } from './loader/loader.component';
 import { ComparatorComponent } from './comparator/comparator.component';
 import { LiveMatchComponent } from './live-match/live-match.component';
 import { BlogPageComponent } from './blog-page/blog-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -26,12 +27,13 @@ import { BlogPageComponent } from './blog-page/blog-page.component';
     LoaderComponent,
     ComparatorComponent,
     LiveMatchComponent,
-    BlogPageComponent
+    BlogPageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([]),
+    AppRoutingModule,
   ],
   providers: [ { provide: HTTP_INTERCEPTORS, useClass: MyHttpInterceptor, multi: true }],
   bootstrap: [AppComponent]
