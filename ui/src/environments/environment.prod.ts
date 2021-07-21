40c54b2ec233e36cd3e987ac48688db5c892d032
yogesh8177 <yogesh.jadhav8177@gmail.com>
2021-07-21 22:17:22 +0530

    fix staging api

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   autoCompleteUrl: 'https://heroes-search.herokuapp.com/auto-complete',
   feedConfigApiUrl: 'https://heroes-search.herokuapp.com/config',
   liveMatchUrl: 'https://heroes-search.herokuapp.com/live-match',
-  fetchVotesUrl: 'https://heroes-search.herokuapp.com/fetch-votes',
-  castVoteUrl: 'https://heroes-search.herokuapp.com/cast-vote',
+  fetchVotesUrl: 'https://heroes-voting-service.herokuapp.com/fetch-votes',
+  castVoteUrl: 'https://heroes-voting-service.herokuapp.com/cast-vote',
   production: true
 };
